d6daf0d3e7e6c8ced17c2bc69b9e8be931ef4013
John Gilmore Bush <john.gilmore.bush@gmail.com>
2015-03-25 18:44:55 -0400

    Can now read from a file and write to output

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #define BUFFER_SIZE 1024
 
-int catfile(FILE *fin);
+int catfile(FILE *fin, FILE *fout);
 
 int main(int argc, char *argv[])
 {
@@ -23,16 +24,23 @@ int main(int argc, char *argv[])
      if((fout = fopen(dev_null_path, "w")) == NULL)
         perror("Failed to open /dev/null for writing\n");
 
-     return catfile(fin);
+     return catfile(fin, fout);
 }
 
-int catfile(FILE *fin)
+int catfile(FILE *fin, FILE *fout)
 {
+    int read = 0;
     char buffer[BUFFER_SIZE];
 
-    while(fread(buffer, sizeof(char), BUFFER_SIZE, fin) != 0)
+    memset(buffer, 0, BUFFER_SIZE);  
+    while((read = fread(buffer, sizeof(char), BUFFER_SIZE, fin)) != 0)
     {
-        
+        if(fwrite(buffer, sizeof(char), BUFFER_SIZE, fout) < read) 
+        {
+            if(ferror(fout))
+                printf("Error writing to /dev/null");
+        }
+        memset(buffer, 0, BUFFER_SIZE);  
     }
 
     // Check to see if we ended because of error
